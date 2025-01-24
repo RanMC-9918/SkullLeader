13ec8410e03ce48b86a32a97076b8181d5b539ca
RanMC-9918 <rohanjain0725@gmail.com>
2025-01-23 22:13:48 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+let skullyMessages = [];
+
+let skullyFormatted = [];
+
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'views')));
+
+app.get("/", (req, res) => {
+  res.render('index', {
+    messages: skullyFormatted
+});
+
+});
+
+// Handle POST requests to /submit
+app.post("/submit", (req, res) => {
+  const data = req.body;
+  console.log("Received data:", data);
+  skullyFormatted.push({content: data.content, author: data.author });
+  res.send("POST request received");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+  
+});
